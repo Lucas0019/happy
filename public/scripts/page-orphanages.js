4c70f36f90b94bf0ec515a3b7b573292d1fe2084
Lucas <lxavier.00@outlook.com>
2020-10-14 08:16:05 -0300

    Estilizaçao do CSS - Maps inicianda

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,5 +1,32 @@
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+// Mapa - Leaflet.js
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+//Create Map
+const map = L.map('mapid').setView([-23.7137158, -46.4167772], 15);
+
+//Create end add tilelayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', ).addTo(map);
+
+
+//Create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+//Create popup
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeigth: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg" </a>')
+
+
+
+//Create end add marker
+L.marker([-23.7137158, -46.4167772], { icon })
+    .addTo(map)
+    .bindPopup(popup)
+    // .openPopup(popup);
