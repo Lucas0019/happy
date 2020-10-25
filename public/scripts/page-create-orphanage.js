5b6cd2e3657e822a9d40c4e881afe05f2325bf30
Lucas <lxavier.00@outlook.com>
2020-10-24 22:00:44 -0300

    Em andamento - construçao do Mapa

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,36 @@
+// Mapa - Leaflet.js
+
+//Create Map
+const map = L.map('mapid').setView([-23.7137158, -46.4167772], 15);
+
+//Create end add tilelayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', ).addTo(map);
+
+
+//Create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+
+})
+
+
+let marker;
+
+map.on("click", (event) => {
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+
+
+    document.querySelector("[name=lat]").value = lat;
+    document.querySelector("[name=lng]").value = lng;
+
+    // Remove icon
+    marker && map.removeLayer(marker);
+
+    // add icon layer
+    marker = L.marker([lat, lng], { icon }).addTo(map);
+});
+
+// Aula 03 - 1:08:16
